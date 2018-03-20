8f019860f6c6ef6c47eef06476e8d527bfb24498
Adam Siefkas <aeos42@Adams-MBP.research>
2018-03-20 15:50:02 -0600

    initial commit

---
+++ background.js
@@ -0,0 +1,22 @@
+var correctPort;
+
+chrome.app.runtime.onLaunched.addListener(function() {
+    chrome.app.window.create('serial.html', {
+      'outerBounds': {
+        'width': 400,
+        'height': 500
+      }
+    });
+    chrome.serial.getDevices(function deviceList(foundPorts) {
+        foundPorts.forEach(port => {
+            if (port.path == "/dev/cu.usbmodem1412")
+            {
+                correctPort = port;
+            }
+        });
+
+
+    });
+
+
+  });
